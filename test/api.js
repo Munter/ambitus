af66d782d56dc4c6cad1ea6f53aad8840d748b9a
Peter Müller <munter@fumle.dk>
2015-02-01 14:59:22 +0100

    Improve api testing

--- test/api.js
+++ test/api.js
@@ -11,6 +11,13 @@ describe('API', function () {
     });
 
     it('should have the described methods', function () {
-        expect(Ambitus.prototype, 'to have keys', ['get', 'interval', 'next', 'previous', 'today', 'go']);
+        expect(Ambitus.prototype, 'to only have keys', ['get', 'interval', 'next', 'previous', 'today', 'go']);
+
+        expect(Ambitus.prototype.get, 'to have arity', 0);
+        expect(Ambitus.prototype.interval, 'to have arity', 1);
+        expect(Ambitus.prototype.next, 'to have arity', 0);
+        expect(Ambitus.prototype.previous, 'to have arity', 0);
+        expect(Ambitus.prototype.today, 'to have arity', 0);
+        expect(Ambitus.prototype.go, 'to have arity', 1);
     });
 });
