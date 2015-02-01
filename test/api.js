95c0835aaf798f5170ba5b06b2faf88efa1eb849
Peter Müller <munter@fumle.dk>
2015-02-01 22:23:30 +0100

    Test event handlers

--- test/api.js
+++ test/api.js
@@ -1,13 +1,18 @@
 var Ambitus = require('../');
-var expect = require('unexpected');
+var moment = require('moment');
+var sinon = require('sinon');
+var expect = require('unexpected').clone();
+
+require('moment-range');
+expect.installPlugin(require('unexpected-sinon'));
 
 describe('API', function () {
     beforeEach(function () {
-        this.ambitus = new Ambitus({
+        this.instance = new Ambitus({
             interval: 'week'
         });
 
-        this.ambitus.go('2000-01-01');
+        this.instance.go('2000-01-01');
     });
 
     it('should have the described methods', function () {
@@ -19,5 +24,53 @@ describe('API', function () {
         expect(Ambitus.prototype.previous, 'to have arity', 0);
         expect(Ambitus.prototype.today, 'to have arity', 0);
         expect(Ambitus.prototype.go, 'to have arity', 1);
+    });
+
+
+    describe('event handling', function () {
+        it('should call the onBeforeChange handler when changing the range', function () {
+            var stub = sinon.stub();
+            var instance = new Ambitus({
+                interval: 'week',
+                onBeforeChange: stub
+            });
+
+            // var before = instance.get();
+            var result = instance.next();
+
+            expect(stub, 'was called');
+            expect(result.range.contains(moment().add(7, 'days')), 'to be true');
+        });
+
+        it('should call the onChange handler when changing the range', function () {
+            var stub = sinon.stub();
+            var instance = new Ambitus({
+                interval: 'week',
+                onChange: stub
+            });
+
+            var result = instance.next();
+
+            expect(stub, 'was called');
+            expect(result.range.contains(moment().add(7, 'days')), 'to be true');
+        });
+
+        it('should not call the onChange handler when changing the range when onBeforeChange handler returns false', function () {
+            var beforeStub = sinon.stub().returns(false);
+            var stub = sinon.stub();
+            var instance = new Ambitus({
+                interval: 'week',
+                onBeforeChange: beforeStub,
+                onChange: stub
+            });
+
+            var before = instance.get();
+            var result = instance.next();
+
+            expect(beforeStub, 'was called');
+            expect(stub, 'was not called');
+            expect(result.range.contains(moment()), 'to be true');
+            expect(result.range.isSame(before.range), 'to be true');
+        });
     });
 });
